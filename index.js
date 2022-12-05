b0317cc8c11178389306dd8dac197cf1d541f481
Nma Shawkat <nmashawket@gmail.com>
2022-12-05 23:40:27 +0300

    doe

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+
 function isPalindrome(word) {
   // Write your algorithm here
 }
 
 /* 
   Add your pseudocode here
+  1. write a function isPalindrome that takes in 1 string as an argument
+  2. Function must return true if the string is a palindrome. Return false if not palindrome
 */
 
 /*
   Add written explanation of your solution here
+  
 */
+function isPalindrome(string) {
+  const reversed = string.split("").reverse().join("");
+  if (string === reversed) return true;
+  else return false;
+}
+
+isPalindrome("racecar");
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
